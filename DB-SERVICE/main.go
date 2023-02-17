0d0dea30f4f2b3f87a56fe4283a36fb5c3f7b8bd
Tosha_Rotten <kozlovad@mer.ci.nsu.ru>
2023-02-18 03:05:45 +0700

    some changes

--- DB-SERVICE/main.go
+++ DB-SERVICE/main.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"flag"
-	. "main/loger"
+	"main/grpc-server"
+	. "main/logger"
 )
 
 var (
-	configPath string
+	LoggerConfigPath   string
+	DatabaseConfigPath string
+	ServerConfigPath   string
 )
 
 func init() {
-	flag.StringVar(&configPath, "configurator-path", "configs/apiserver.yaml", "path to configurator file")
+	flag.StringVar(&LoggerConfigPath, "PathConfigLogger", "configs/logger-config.yaml", "Path to logger config file")
+	flag.StringVar(&DatabaseConfigPath, "PathConfigDatabase", "configs/database-config.yaml", "Path to database config file")
+	flag.StringVar(&ServerConfigPath, "PathConfigServer", "configs/server-config.yaml", "Path to server config file")
 }
 
 func main() {
 	flag.Parse()
-	GlobalLoger.Info("Starting ...")
+	logger := New()
+	logger.Configurate(LoggerConfigPath)
+	logger.Log.Info("Starting ...")
+	server := grpc_server.New()
+
+	err := server.Configurate(ServerConfigPath)
+	if err != nil {
+		logger.Log.Error(err)
+	}
+	err = server.Start()
+	if err != nil {
+		logger.Log.Error(err)
+	}
 }
