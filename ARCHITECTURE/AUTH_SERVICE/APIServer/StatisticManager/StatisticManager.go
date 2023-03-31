c52f0af7937188743ec845ff00fa66be4faae4d4
Tosha_Rotten <kozlovad@mer.ci.nsu.ru>
2023-03-31 23:46:05 +0700

    statistic manager for statistic service

---
+++ ARCHITECTURE/AUTH_SERVICE/APIServer/StatisticManager/StatisticManager.go
@@ -0,0 +1,11 @@
+package StatisticManager
+
+import "github.com/ToshaRotten/open-educaton-management-system/ARCHITECTURE/AUTH_SERVICE/APIServer/APIServer/StatisticManager/models"
+
+type StatisticManager struct {
+	Last models.Stats
+}
+
+func New() *StatisticManager {
+	return &StatisticManager{}
+}
