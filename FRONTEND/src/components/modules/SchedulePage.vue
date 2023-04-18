e37d4e5f9865bf9378576d681eac473849f7d956
ro_sv <ro.sv404@gmail.com>
2023-04-18 21:52:58 +0700

    some fixed bugs

--- FRONTEND/src/components/modules/SchedulePage.vue
+++ FRONTEND/src/components/modules/SchedulePage.vue
@@ -1,21 +1,24 @@
 <template>
-a
+	<FullCalendar v-bind:options="options"/>
 </template>
+<script setup>
+// import '@fullcalendar/core/vdom'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import listPlugin from '@fullcalendar/list'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import FullCalendar from '@fullcalendar/vue3'
+import { reactive } from 'vue'
 
-<script>
-
-export default {
-	name: 'ModulesPage',
-	data() {
-			return {
-					
-			}
-	},
-
-}
+const options = reactive({
+	plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
+	initialView: 'dayGridMonth',
+	editable: true,
+	selectable: true,
+	weekends: true,
+	select: (arg) => {
+		
+	}
+})
 
 </script>
-<style>
-
-
-</style>
