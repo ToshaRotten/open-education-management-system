4983a36c7e10c52e950a88528dd3ed7a4c6e4748
ro_sv <ro.sv404@gmail.com>
2023-04-19 19:32:33 +0700

    schedule

--- FRONTEND/src/components/modules/SchedulePage.vue
+++ FRONTEND/src/components/modules/SchedulePage.vue
@@ -8,8 +8,85 @@
 			<div class='drag'></div>
 		</div>
 	</div>
-	<FullCalendar :options="options"/>
-	<EditLesson/>
+
+	<div style="position: relative;">
+		<FullCalendar id="elem1" :options="options"/>
+		<template id="elem2">
+			<v-row justify="center">
+				<v-dialog
+						v-model="edit"
+						persistent
+						width="600"
+						height="512"
+				>
+					<template v-slot:activator="{ props }">
+						<v-btn
+								color="primary"
+								v-bind="props"
+						>
+							Open Dialog
+						</v-btn>
+					</template>
+					<v-card>
+						<v-card-title>
+							<span class="text-h5">Редактирование урока</span>
+						</v-card-title>
+						<v-card-text>
+							<v-container>
+								<v-row>
+									<v-col
+											cols="12"
+											sm="6"
+											md="4"
+									>
+										<v-text-field
+												label="Название"
+												required
+										></v-text-field>
+									</v-col>
+									<v-col
+											cols="12"
+											sm="6"
+											md="4"
+									>
+										<v-text-field
+												label="Описание"
+												hint="example of helper text only on focus"
+										></v-text-field>
+									</v-col>
+									<v-col
+											cols="12"
+											sm="6"
+									>
+										<v-datetime-picker label="Select Datetime" v-model="datetime"> </v-datetime-picker>
+									</v-col>
+								</v-row>
+							</v-container>
+							<small>*indicates required field</small>
+						</v-card-text>
+						<v-card-actions>
+							<v-spacer></v-spacer>
+							<v-btn
+									color="blue-darken-1"
+									variant="text"
+									@click="edit = false"
+							>
+								Close
+							</v-btn>
+							<v-btn
+									color="blue-darken-1"
+									variant="text"
+									@click="edit = false"
+							>
+								Сохранить
+							</v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-dialog>
+			</v-row>
+		</template>
+	</div>
+
 	
 </template>
 <script>
@@ -19,15 +96,16 @@ import interactionPlugin, { Draggable } from '@fullcalendar/interaction'
 import listPlugin from '@fullcalendar/list'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import FullCalendar from '@fullcalendar/vue3'
-import EditLesson from './EditLesson.vue'
+import {INITIAL_EVENTS} from "./events";
 
 export default {
   components: {
     FullCalendar, // make the <FullCalendar> tag available
-		EditLesson
+
   },
   data() {
     return {
+			datetime: '',
 			lessons: [
 				{ 
 					"title": "Русский язык", 
@@ -40,6 +118,7 @@ export default {
 					"backgroundColor": "#3FBAFF" 
 				}
 			],
+			edit: false,
       options: {
         plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
 				initialView: 'timeGridWeek',
@@ -58,27 +137,13 @@ export default {
 				dateClick: this.handleDateClick,
 				weekends: true,
 				firstDay: '1',
-				events: [
-					{
-						id: 0,
-						title: 'Русский язык',
-						start: '2023-04-15T10:30:00',
-						end: '2023-04-15T12:30:00'
-					},
-					{
-						id: 1,
-						title: 'Литература',
-						start: '2023-04-19T10:00:00',
-						end: '2023-04-19T10:45:00'
-					},
-				],
-				// eventDrop: function(info) {
-				// 	alert(info.event.title + " was dropped on " + info.event.start.toISOString() + "and old event is" + info.oldEvent.start.toISOString());
-
-				// 	if (!confirm("Are you sure about this change?")) {
-				// 		info.revert();
-				// 	}
-				// },
+				events: INITIAL_EVENTS,
+				eventClick: (info) => {
+					console.log(this.edit)
+					this.edit = true
+					console.log(this.edit)
+					console.log('View: ' + info.event.title)
+				},
 				buttonText: {
 					today:    'Сегодня',
 					month:    'Месяц',
@@ -88,21 +153,23 @@ export default {
 				},
 				height: 700,
 				locale: 'ru',
-				themeSystem: 'bootstrap4'
+				themeSystem: 'bootstrap5'
       }
     }
   },
   methods: {
     handleDateClick: function(arg) {
       console.log('date click! ' + arg.dateStr)
-    }
+    },
+		showModal() {
+			return this.edit = !this.edit
+		}
   },
 	created: function() {
 		let array = this.lessons
 		document.addEventListener('DOMContentLoaded', function() {
 		let draggableEl = document.getElementsByClassName("drag");
 		for(let i = 0; i < array.length; i++) {
-			console.log(array[i])
 			new Draggable(draggableEl[i], {
 				eventData: array[i]
 			});
@@ -128,5 +195,15 @@ export default {
 	border-radius: 10px;
 	border-color: white;
 }
-
+#elem1,
+#elem2 {
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		top: 0;
+		left: 0;
+}
+#elem2 {
+	z-index: 10;
+}
 </style>
--- FRONTEND/src/main.js
+++ FRONTEND/src/main.js
@@ -5,10 +5,11 @@ import * as directives from 'vuetify/directives'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
+import DatetimePicker from 'vuetify-datetime-picker'
 
 const vuetify = createVuetify({
   components,
   directives,
 })
 
-createApp(App).use(router).use(store).use(vuetify).mount('#app')
+createApp(App).use(router).use(store).use(vuetify).use(DatetimePicker).mount('#app')
