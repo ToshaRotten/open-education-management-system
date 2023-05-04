d50e6ddc9550a18f061c150e67bfa4dda3e40bba
Tosha_Rotten <kozlovad@mer.ci.nsu.ru>
2023-05-04 20:30:51 +0700

    db controller

---
+++ ARCHITECTURE/AUTH_SERVICE/database/controller_test.go
@@ -0,0 +1,19 @@
+package database
+
+import "testing"
+
+func TestDBController_Create(t *testing.T) {
+
+}
+
+func TestDBController_Read(t *testing.T) {
+
+}
+
+func TestDBController_Update(t *testing.T) {
+
+}
+
+func TestDBController_Delete(t *testing.T) {
+
+}
